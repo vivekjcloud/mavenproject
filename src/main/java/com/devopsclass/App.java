e25186bc78337cf546884ef96fb8cafcc36e5d99
moula-shaik <115640750+moula-shaik@users.noreply.github.com>
2022-10-29 15:45:15 +0530

    updated App.java file

--- src/main/java/com/devopsclass/App.java
+++ src/main/java/com/devopsclass/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! , updated a file from github" );
+        System.out.println( "Hello World! , updated a file in 2nd time " );
     }
 }
