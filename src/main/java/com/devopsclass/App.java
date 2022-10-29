31767d2949fdfd6e9896892354a0142a6694909e
moula-shaik <115640750+moula-shaik@users.noreply.github.com>
2022-10-29 15:38:58 +0530

    updated App.java file

--- src/main/java/com/devopsclass/App.java
+++ src/main/java/com/devopsclass/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World! , updated a file from github" );
     }
 }
