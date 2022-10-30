e1271787ba02257ed8991626b5355867519b8363
moula-shaik <115640750+moula-shaik@users.noreply.github.com>
2022-10-30 21:40:23 +0530

    updated App.java

--- src/main/java/com/devopsclass/App.java
+++ src/main/java/com/devopsclass/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! , trigger for to pooling the code from github , 4TH TIME TRIGGEREd" );
+        System.out.println( "Hello World! , trigger for to pooling the code from github , 5TH TIME TRIGGEREd" );
     }
 }
