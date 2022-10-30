f06ca4ec56f2c28f79422652d2cd3b31690ed85f
moula-shaik <115640750+moula-shaik@users.noreply.github.com>
2022-10-30 09:55:18 +0530

    updated App.java

--- src/main/java/com/devopsclass/App.java
+++ src/main/java/com/devopsclass/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! , trigger sucessfully made" );
+        System.out.println( "Hello World! , trigger for to pooling the code from github" );
     }
 }
